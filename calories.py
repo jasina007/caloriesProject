17cdf94f8674a83aa6d29f0077a5e937c36d39cc
jasina007 <jasina007@github.com>
2023-06-01 12:58:45 +0200

    all functions needed to GUI done, but in 1 file

--- calories.py
+++ calories.py
@@ -1,10 +1,11 @@
 import re
 from sklearn.linear_model import LinearRegression
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import date
 
 def splitAndConvertToInt(listOfSplittedRow, rowIndex):
     return int(re.split(' ', listOfSplittedRow[rowIndex])[0])
-    
     
 #data about calories are in order: (food name, food type, cal, kJ, g/ml)
 def loadCaloriesData():
@@ -44,6 +45,7 @@ def loadCaloriesData():
             #add new tuple to list
             caloriesList.append((nameFood, typeFood, caloriesAmount, kilojoulesAmount, unit))
     return caloriesList #returning a created list of tuples
+
 
 #below functions are a part of 'loadActivitiesData()' (to avoid repeating the same code and improve the readiness)
 
@@ -141,7 +143,6 @@ def getMinMaxEqSpeedInRow(nameAndTypeActivity):
                                 
     return minSpeedMph, maxSpeedMph, eqSpeedMph
 
-
 #method which splits a row, giving a type and type of activity, giving speeds and calories amounts 
 def splitRowAndGetData(row, signToSplit, indexToSplitRow, indexToSplitTypeActivity):
     splittedRow = re.split(signToSplit, row)
@@ -161,12 +162,12 @@ def getActivityName(processedTypeActivity):
 def addActivityToDict(activityName, activitiesDict, row, slope, minSpeed, maxSpeed, eqSpeed):
     if activityName in activitiesDict:
         if row.__contains__('mph'):
-            activitiesDict[activityName].append([(minSpeed, maxSpeed, eqSpeed, slope)])
+            activitiesDict[activityName].append((minSpeed, maxSpeed, eqSpeed, slope))
     else:   
         if row.__contains__('mph'):
             activitiesDict.update({activityName: [(minSpeed, maxSpeed, eqSpeed, slope)]})
         else:
-            activitiesDict.update({activityName: [(slope)]})
+            activitiesDict.update({activityName: slope})
 
 
 #method which load 'exercise_dataset.csv' file and process data 
@@ -195,15 +196,79 @@ def loadActivitiesData():
             slope = float(caloriesLinearRegression(convert1DListTo2D(arguments)))   
             
             #convert all speeds to km/h       
-            minSpeed, maxSpeed, eqSpeed = convertMinMaxEqToKmh(minSpeedMph, maxSpeedMph, eqSpeedMph)
+            minSpeedKmh, maxSpeedKmh, eqSpeedKmh = convertMinMaxEqToKmh(minSpeedMph, maxSpeedMph, eqSpeedMph)
             
             #check conditions and add new sport activity to a dictionary
-            addActivityToDict(activityName, activitiesDict, row, slope, minSpeed, maxSpeed, eqSpeed)
+            addActivityToDict(activityName, activitiesDict, row, slope, minSpeedKmh, maxSpeedKmh, eqSpeedKmh)
                         
     return activitiesDict
 
+
+def countBMR(sex, weight, height, age):
+    match sex:
+        case 'female':
+            return 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+        case 'male':
+            return 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+        case _:
+            return None
+
+def percentCaloriesInBMR(caloriesAmount, bmr):
+    return caloriesAmount/bmr * 100
+
+#structure of caloriesEaten: [(caloriesAmount, name of food)]
+def pieChartWithCalories(caloriesEaten, bmr):
+    #taking calories amounts and food names from a list of tuples
+    caloriesAmounts = [percentCaloriesInBMR(elem[0], bmr) for elem in caloriesEaten]
+    foodNames = [elem[1] for elem in caloriesEaten]
+
+    #check if the eaten food exceeds 100%
+    #if not, it will be showed in a pie chart
+    sumCalories = sum(caloriesAmounts)
+    if sumCalories < 100:
+        unEatenPercent = 100 - sumCalories
+        caloriesAmounts.append(unEatenPercent)
+        foodNames.append('Not eaten')
+
+    #set colours on a pie chart
+    colours = sns.color_palette('bright')
+    
+    #set settings of a pie chart and percents to 2 decimal points
+    plt.pie(caloriesAmounts, labels=foodNames, colors=colours, autopct= '%.2f%%')
+    plt.show()
+
+
+def findFoodNamesByString(listCalories, enteredString):
+    listMatchingFoods = []
+    
+    #to remind, listCalories is of a structure: [(food name, food type, cal, kJ, g/ml)]
+    for food in listCalories:
+        #transform string to lowercase to avoid mis-match
+        if enteredString.lower() in food[0].lower():
+            listMatchingFoods.append(food)
+    return listMatchingFoods
+
+
+def findActivitiesByString(dictActivities, enteredString):
+    dictMatchingActivities = dict()
+    
+    #to remind, dictActivities contains: {activityName: [(minSpeedKmh, maxSpeedKmh, eqSpeedKmh, slope)]} or {activityName: slope}
+    for key, value in dictActivities.items():
+        if enteredString.lower() in key.lower():
+            dictMatchingActivities.update({key: value})
+    return dictMatchingActivities
+    
+    
 if __name__ == '__main__':
     #print(takeFirstNumberFromString(string))
-    dict1 = loadActivitiesData()
+    '''dict1 = loadActivitiesData()
     for item in dict1.items():
-        print(item)
+        print(item)'''
+    dictActivities = {'Sledding, tobagganing, luge': 6.993062956504314, 'Cleaning gutters': 5.000084106353022, 
+                     'Walking': [(None, 3.218688, None, 1.9929788501512924), (None, None, 3.218688, 2.495632807932813), 
+                                (None, None, 4.02336, 2.998286765714334), (None, None, 4.828032, 3.298115442285767), 
+                                (None, None, 5.632704, 3.827224871529473), (None, None, 5.632704, 5.996573531428668), 
+                                (None, None, 6.437376, 5.000084106353022), (None, None, 7.2420480000000005, 6.2964022080001), 
+                                (None, None, 8.04672, 8.02482634352954)]}
+    print(findActivitiesByString(dictActivities, 'walk'))
+    
