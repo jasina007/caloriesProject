ceb6c6806348d4ca9c6fc11966dd1de529ebce7c
jasina007 <jasina007@github.com>
2023-06-01 18:08:01 +0200

    beggining gui

--- gui.py
+++ gui.py
@@ -1,2 +1,20 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout
+
+class CaloriesAndActivities(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Calories project')
+        
+        
+        
+        
+        
+        self.userDataLayout = QHBoxLayout()
+        
+        
+        
+app = QApplication(sys.argv)
+window = QMainWindow()
+window.show()
+app.exec()
