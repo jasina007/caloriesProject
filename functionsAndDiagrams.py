299cf169fd91c8e5eabaf44a2e27b3896301d6e6
jasina007 <jasina007@github.com>
2023-06-01 17:11:09 +0200

    added getting slope by speed

--- functionsAndDiagrams.py
+++ functionsAndDiagrams.py
@@ -57,11 +57,56 @@ def findActivitiesByString(dictActivities, enteredString):
     return dictMatchingActivities
 
 
+#it is the way to sort a list with tuples with speeds
+def findMinInSpeedTuple(tuple):
+    smallest = float('inf')  # Infinity because every existing number is smaller than it
+    
+    # Iterate over the elements in the tuple(without the last one, slope)
+    for element in tuple[:3]:
+        if element is not None and element < smallest:
+            smallest = element
+    return smallest
+
+
+# Sort the list of tuples based on the smallest number in each tuple(without slopes)
+def sortSpeedValues(dictValueSpeed):
+    sorted_data = sorted(dictValueSpeed, key=findMinInSpeedTuple)
+    return sorted_data
+
+
+#define what is a minimum speed what user can enter(by a widget)
+def getMinSpeedEnteredByUser(listTupleSpeeds):
+    sortedListSpeeds = sortSpeedValues(listTupleSpeeds)
+    firstTupleSpeed = sortedListSpeeds[0]
+    minSpeedTuple = findMinInSpeedTuple(firstTupleSpeed)
+    indexMinSpeed = firstTupleSpeed.index(minSpeedTuple)
+    
+    #there are only 2 possibilities: maxSpeed(index == 1) or eqSpeed(index==2)
+    match indexMinSpeed:
+        case 2: 
+            return minSpeedTuple
+        case 1: 
+            return 0
+        case _:
+            return None
+
+
+#get a slope of a speed defined by a user from specified interval defined in activities data
+def getSlopeFromCorrectSpeedInterval(listTupleSpeeds, speedKmh):
+    sortedListSpeeds = sortSpeedValues(listTupleSpeeds)
+    currentIndex = 1
+    
+    #check if a speed entered by user belongs to current interval, if not, index is increasing
+    while(currentIndex < len(sortedListSpeeds)):
+        if speedKmh < findMinInSpeedTuple(sortedListSpeeds[currentIndex]):
+            return sortedListSpeeds[currentIndex-1][-1]
+        currentIndex += 1
+        
+    #if a speed is bigger than all intervals, the returning slope is from the last tuple
+    if currentIndex == len(sortedListSpeeds):
+        return sortedListSpeeds[currentIndex-1][-1]
+        
+
 if __name__ == '__main__':
-    dictActivities = {'Sledding, tobagganing, luge': 6.993062956504314, 'Cleaning gutters': 5.000084106353022, 
-                     'Walking': [(None, 3.218688, None, 1.9929788501512924), (None, None, 3.218688, 2.495632807932813), 
-                                (None, None, 4.02336, 2.998286765714334), (None, None, 4.828032, 3.298115442285767), 
-                                (None, None, 5.632704, 3.827224871529473), (None, None, 5.632704, 5.996573531428668), 
-                                (None, None, 6.437376, 5.000084106353022), (None, None, 7.2420480000000005, 6.2964022080001), 
-                                (None, None, 8.04672, 8.02482634352954)]}
-    print(findActivitiesByString(dictActivities, 'lug'))
+    dictActivities = [(None, 3.218688, None, 1.9929788501512924), (None, None, 3.218688, 2.495632807932813), (None, None, 4.02336, 2.998286765714334), (None, None, 4.828032, 3.298115442285767), (None, None, 5.632704, 3.827224871529473), (None, None, 5.632704, 5.996573531428668), (None, None, 6.437376, 5.000084106353022), (None, None, 7.2420480000000005, 6.2964022080001), (None, None, 8.04672, 8.02482634352954)]
+    print(getSlopeFromCorrectSpeedInterval(dictActivities, 3.4))
